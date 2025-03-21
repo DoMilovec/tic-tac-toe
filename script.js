749caae49be5a40f3d74f35fa0e860acfea91957
DoMilovec <dmilovec@gmail.com>
2025-03-21 18:52:13 +0100

    Add draw board logic, random X and random O funcs
    Working on console version, added function to draw
    an empty board (filled with n's) and functions to add
    X and O to random empty('n') spots

---
+++ script.js
@@ -0,0 +1,54 @@
+const Gameboard = function () {
+    let board = [];
+    const cell = 'n';
+
+    // draws empty board
+    const drawBoard = function () {
+        for (let i = 0; i < 3; i++) {
+            board[i] = [];
+            for (let j = 0; j < 3; j++) {
+                board[i].push(cell);
+            }
+        }
+    }
+
+    // for console testing:
+    // looks for coordinates of value 'n' and saves to array emptyCells
+    const playX = function() {
+        let emptyCells = [];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] === 'n') {
+                    emptyCells.push([i, j]);
+                }
+            }
+        }
+        // if emptyCells isnt 0 (if there are 'n' values), select random index in it
+        // and save x and y coordinates with const [x, y] by randomizing emptyCells index
+        // then update board with X on those x and y coordinates
+        if (emptyCells.length > 0) {
+            const [x, y] = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+            board[x][y] = 'X';
+        }
+    }
+    const playO = function() {
+        let emptyCells = [];
+        for (let i = 0 ; i < 3 ; i++){
+            for (let j = 0 ; j < 3 ; j++) {
+                if (board[i][j] === 'n') {
+                    emptyCells.push([i, j]);
+                }
+            }
+        }
+        if (emptyCells.length > 0){
+            const [x, y] = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+            board[x][y] = 'O';
+        }
+    }
+
+    drawBoard();
+    return { board, playX, playO }
+}
+
+const game = Gameboard();
+console.log(game.board);
